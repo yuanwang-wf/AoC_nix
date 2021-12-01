84e3e7cef1d76a8e8fff94958a7e7ef5c157a504
yuanwang <yuan.wang@workiva.com>
2021-12-01 07:06:07 -0600

    add day 1

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,12 +1,24 @@
 module Day1 where
 
 readInput :: FilePath -> IO [Int]
-readInput path = (map readNum . lines) <$> readFile path
+readInput path = map readNum . lines <$> readFile path
   where
     readNum :: String -> Int
     readNum = read
 
+getNums :: IO [Int]
+getNums = readInput "data/day1.txt"
+
 partI :: IO ()
 partI = do
-  nums <- readInput "data/input1.txt"
-  print nums
+  nums <- getNums
+  let m = fst $ foldl (\(counter, prev) num -> (counter + if maybe False ( < num) prev  then 1 else 0 , Just num)) (0, Nothing) nums
+  print m
+
+partII :: IO ()
+partII = do
+  nums <- getNums
+  print (myFold nums)
+
+myFold :: [Int] -> Int
+myFold nums = length  [i | i <- [0.. length nums - 4 ], nums !! i < nums !! (i+3)]
