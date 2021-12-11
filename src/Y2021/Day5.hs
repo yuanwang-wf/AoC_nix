2d18cfb02264cb79591a6a95ceb76a34bd6d0c06
yuanwang <yuan.wang@workiva.com>
2021-12-11 13:32:48 -0600

    format

--- src/Y2021/Day5.hs
+++ src/Y2021/Day5.hs
@@ -1,10 +1,10 @@
-module Y2021.Day5 ( day5PartII, day5PartI) where
+module Y2021.Day5 (day5PartII, day5PartI) where
 
 import Control.Arrow ((&&&))
 import Control.Monad (join)
-import Data.Array ( array, Array, (//), (!), elems )
+import Data.Array (Array, array, elems, (!), (//))
+import Data.Bits (Bits (xor))
 import Data.List.Split (splitOn)
-import Data.Bits (Bits(xor))
 import qualified Data.Map.Strict as Map
 
 type Point = (Int, Int)
@@ -12,72 +12,72 @@ type Point = (Int, Int)
 type Vent = (Point, Point)
 
 readVent :: String -> Vent
-readVent =  ((nums . (!! 0))   &&& (nums . (!! 1)))    . splitOn "->"
+readVent = ((nums . (!! 0)) &&& (nums . (!! 1))) . splitOn "->"
   where
-        nums :: String -> Point
-        nums =  ((readInt . (!! 0) ) &&& ( readInt  . (!! 1 )))   . splitOn ","
+    nums :: String -> Point
+    nums = ((readInt . (!! 0)) &&& (readInt . (!! 1))) . splitOn ","
 
-        readInt :: String -> Int
-        readInt = read
+    readInt :: String -> Int
+    readInt = read
 
 grid :: Array (Int, Int) Int
-grid = array ((0,0), (999, 999)) [((i,j), 0) | i <- [0..999], j <- [0..999]]
+grid = array ((0, 0), (999, 999)) [((i, j), 0) | i <- [0 .. 999], j <- [0 .. 999]]
 
-updateGrid :: Array (Int, Int) Int  -> Point -> Point -> Array (Int, Int) Int
-updateGrid grid (a,b) (a', b') = grid // [ ((i,j),  1 + (grid ! (i,j)) )   | i <-  [a .. a'], j <- [b .. b'] ]
+updateGrid :: Array (Int, Int) Int -> Point -> Point -> Array (Int, Int) Int
+updateGrid grid (a, b) (a', b') = grid // [((i, j), 1 + (grid ! (i, j))) | i <- [a .. a'], j <- [b .. b']]
 
 testVent :: [Vent]
-testVent = [((0,9), (5,9)),
-            ((8,0), (0,8)),
-            ((9,4), (3,4)),
-            ((2,2), (2,1)),
-            ((7,0), (7,4)),
-            ((6,4), (2,0)),
-            ((0,9), (2,9)),
-            ((3,4), (1,4)),
-            ((0,0), (8,8)),
-            ((5,5), (8,2))]
+testVent =
+  [ ((0, 9), (5, 9)),
+    ((8, 0), (0, 8)),
+    ((9, 4), (3, 4)),
+    ((2, 2), (2, 1)),
+    ((7, 0), (7, 4)),
+    ((6, 4), (2, 0)),
+    ((0, 9), (2, 9)),
+    ((3, 4), (1, 4)),
+    ((0, 0), (8, 8)),
+    ((5, 5), (8, 2))
+  ]
 
 partI :: [Vent] -> Int
-partI  =  length  . filter (>= 2)   .  elems . foldr (f . _sort) grid . _filter
-  where _filter :: [Vent] -> [Vent]
-        _filter = filter (\((a,b), (a', b')) -> (a == a') `xor` (b == b'))
+partI = length . filter (>= 2) . elems . foldr (f . _sort) grid . _filter
+  where
+    _filter :: [Vent] -> [Vent]
+    _filter = filter (\((a, b), (a', b')) -> (a == a') `xor` (b == b'))
 
-        _sort :: Vent -> Vent
-        _sort ((a,b), (a',b')) = ((min a a', min b b'), (max a a', max b b'))
+    _sort :: Vent -> Vent
+    _sort ((a, b), (a', b')) = ((min a a', min b b'), (max a a', max b b'))
 
-        f :: Vent -> Array (Int, Int) Int -> Array (Int, Int) Int
-        f v as = updateGrid as `uncurry` v
+    f :: Vent -> Array (Int, Int) Int -> Array (Int, Int) Int
+    f v as = updateGrid as `uncurry` v
 
 day5PartI :: IO Int
-day5PartI =  partI <$> content
-  where content :: IO [Vent]
-        content =  map readVent  .lines  <$> readFile "data/2021/day5.txt"
-
-
-
+day5PartI = partI <$> content
+  where
+    content :: IO [Vent]
+    content = map readVent . lines <$> readFile "data/2021/day5.txt"
 
 range :: (Point, Point) -> [Point]
-range a = if isNotDiagonal a then ( straightRange . _sortS) a  else (if isDiagonal a then (diagonalRange. _sortD) a else []) -- if possible to rewrite it with alernative
-  where isNotDiagonal :: (Point, Point) -> Bool
-        isNotDiagonal =  \ ((a,b), (a', b')) -> (a == a') `xor` (b == b')
+range a = if isNotDiagonal a then (straightRange . _sortS) a else (if isDiagonal a then (diagonalRange . _sortD) a else []) -- if possible to rewrite it with alernative
+  where
+    isNotDiagonal :: (Point, Point) -> Bool
+    isNotDiagonal = \((a, b), (a', b')) -> (a == a') `xor` (b == b')
 
-        isDiagonal :: (Point, Point) -> Bool
-        isDiagonal = \ ((a,b), (a', b')) -> abs (a - a') == abs (b - b')
+    isDiagonal :: (Point, Point) -> Bool
+    isDiagonal = \((a, b), (a', b')) -> abs (a - a') == abs (b - b')
 
-        _sortS :: Vent -> Vent
-        _sortS ((a,b), (a',b')) = ((min a a', min b b'), (max a a', max b b'))
+    _sortS :: Vent -> Vent
+    _sortS ((a, b), (a', b')) = ((min a a', min b b'), (max a a', max b b'))
 
-        _sortD :: Vent -> Vent
-        _sortD ((a,b), (a', b')) = if a > a' then ((a', b'), (a,b)) else ((a,b), (a', b'))
+    _sortD :: Vent -> Vent
+    _sortD ((a, b), (a', b')) = if a > a' then ((a', b'), (a, b)) else ((a, b), (a', b'))
 
-        straightRange :: (Point, Point) -> [Point]
-        straightRange ((a,b), (a', b')) = [(i,j) | i <- [a..a'], j <- [b..b']]
+    straightRange :: (Point, Point) -> [Point]
+    straightRange ((a, b), (a', b')) = [(i, j) | i <- [a .. a'], j <- [b .. b']]
 
-        diagonalRange :: (Point, Point) -> [Point]
-        diagonalRange ((a,b), (a', b')) = let o = if b > b' then (-) else (+)  in   [(a +i, b `o` i) | i <- [ 0 .. (a' - a)]]
-
-
+    diagonalRange :: (Point, Point) -> [Point]
+    diagonalRange ((a, b), (a', b')) = let o = if b > b' then (-) else (+) in [(a + i, b `o` i) | i <- [0 .. (a' - a)]]
 
 -- take me a while to relaized how bad is this method
 -- we are updating grid too many times, and we do not need 1000*1000 grid to begin with
@@ -87,13 +87,14 @@ range a = if isNotDiagonal a then ( straightRange . _sortS) a  else (if isDiagon
 --         f :: Point -> Array (Int, Int) Int -> Array (Int, Int) Int
 --         f (x,y) grid = grid // [((x,y), grid !(x,y) + 1  )]
 
-
 partII :: [Vent] -> Int
-partII = Map.foldr (\ a b -> if a >= 2 then b +1 else b ) 0  . foldr f Map.empty  . concatMap range
+partII = Map.foldr (\a b -> if a >= 2 then b + 1 else b) 0 . foldr f Map.empty . concatMap range
   where
-        f :: Point -> Map.Map (Int, Int) Int -> Map.Map (Int, Int) Int
-        f p m = Map.insertWith (+) p 1 m
+    f :: Point -> Map.Map (Int, Int) Int -> Map.Map (Int, Int) Int
+    f p m = Map.insertWith (+) p 1 m
+
 day5PartII :: IO Int
-day5PartII =  partII <$> content
-  where content :: IO [Vent]
-        content =  map readVent  .lines  <$> readFile "data/2021/day5.txt"
+day5PartII = partII <$> content
+  where
+    content :: IO [Vent]
+    content = map readVent . lines <$> readFile "data/2021/day5.txt"
--- src/Y2021/Day6.hs
+++ src/Y2021/Day6.hs
@@ -1,14 +1,10 @@
-module Y2021.Day6 (day6PartI, day6PartII  ) where
+module Y2021.Day6 (day6PartI, day6PartII) where
 
+import Control.Monad.ST (runST)
 import Data.List.Split (splitOn)
+import Data.STRef (newSTRef, readSTRef, writeSTRef)
 
-import Control.Monad.ST ( runST )
-import Data.STRef ( newSTRef, readSTRef, writeSTRef )
-
-
-
-
-data FS = FS { d0 ::Integer, d1:: Integer, d2:: Integer, d3:: Integer, d4 :: Integer, d5 :: Integer, d6 :: Integer, d7 :: Integer, d8 :: Integer } deriving Show
+data FS = FS {d0 :: Integer, d1 :: Integer, d2 :: Integer, d3 :: Integer, d4 :: Integer, d5 :: Integer, d6 :: Integer, d7 :: Integer, d8 :: Integer} deriving (Show)
 
 empty :: FS
 empty = FS 0 0 0 0 0 0 0 0 0
@@ -17,18 +13,18 @@ value :: FS -> Integer
 value fs = d0 fs + d1 fs + d2 fs + d3 fs + d4 fs + d5 fs + d6 fs + d7 fs + d8 fs
 
 accumFS :: Int -> FS -> FS
-accumFS 0 fs = fs {d0 = d0 fs + 1 }
-accumFS 1 fs = fs {d1 = d1 fs + 1 }
-accumFS 2 fs = fs {d2 = d2 fs + 1 }
-accumFS 3 fs = fs {d3 = d3 fs + 1 }
-accumFS 4 fs = fs {d4 = d4 fs + 1 }
-accumFS 5 fs = fs {d5 = d5 fs + 1 }
-accumFS 6 fs = fs {d6 = d6 fs + 1 }
-accumFS 7 fs = fs {d7 = d7 fs + 1 }
-accumFS _ fs  = fs
+accumFS 0 fs = fs {d0 = d0 fs + 1}
+accumFS 1 fs = fs {d1 = d1 fs + 1}
+accumFS 2 fs = fs {d2 = d2 fs + 1}
+accumFS 3 fs = fs {d3 = d3 fs + 1}
+accumFS 4 fs = fs {d4 = d4 fs + 1}
+accumFS 5 fs = fs {d5 = d5 fs + 1}
+accumFS 6 fs = fs {d6 = d6 fs + 1}
+accumFS 7 fs = fs {d7 = d7 fs + 1}
+accumFS _ fs = fs
 
 startState :: FS
-startState = FS 0 1 1 2 1 0 0 0 0  --[3,4,3,1,2]
+startState = FS 0 1 1 2 1 0 0 0 0 --[3,4,3,1,2]
 
 shift :: FS -> FS
 shift fs = fs {d0 = d1 fs, d1 = d2 fs, d2 = d3 fs, d3 = d4 fs, d4 = d5 fs, d5 = d6 fs, d6 = d7 fs + d0 fs, d7 = d8 fs, d8 = d0 fs}
@@ -40,12 +36,12 @@ fishST :: Integer -> FS -> FS
 fishST day state = runST $ do
   x <- newSTRef state
   fishST' day x
-  where fishST' 0 x = readSTRef x
-        fishST' n x = do
-          x' <- readSTRef x
-          writeSTRef x $! shift x'
-          fishST' (n-1) x
-
+  where
+    fishST' 0 x = readSTRef x
+    fishST' n x = do
+      x' <- readSTRef x
+      writeSTRef x $! shift x'
+      fishST' (n -1) x
 
 day6PartI :: IO Integer
 day6PartI = value . fishST 80 <$> day6Input
