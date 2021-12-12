388971c0ecd90c50756492aa71308abd515ced9f
yuanwang <yuan.wang@workiva.com>
2021-12-12 07:49:30 -0600

    update

--- src/Y2021/Day7.hs
+++ src/Y2021/Day7.hs
@@ -1,6 +1,5 @@
 module Y2021.Day7 (day7PartII, day7PartI) where
 
-
 import Data.List (sort)
 
 day7Input :: IO [Int]
@@ -9,17 +8,17 @@ day7Input = read . (\x -> "[" ++ x ++ "]") <$> readFile "data/2021/day7.txt"
 -- quick selection
 median :: (Ord a, Fractional a) => [a] -> a
 median x =
-   if odd n
-     then sort x !! (n `div` 2)
-     else ((sort x !! (n `div` 2 - 1)) + (sort x !! (n `div` 2))) / 2
-    where n = length x
-
+  if odd n
+    then sort x !! (n `div` 2)
+    else ((sort x !! (n `div` 2 - 1)) + (sort x !! (n `div` 2))) / 2
+  where
+    n = length x
 
 findCost :: Int -> [Int] -> Int
-findCost num  = sum . fmap ( abs . (-) num)
+findCost num = sum . fmap (abs . (-) num)
 
 findCost' :: Int -> [Int] -> Int
-findCost' num  = sum . fmap ((\x -> (x*(x+1)) `div` 2) . ( abs . (-) num))
+findCost' num = sum . fmap ((\x -> (x * (x + 1)) `div` 2) . (abs . (-) num))
 
 findAllCost :: [Int] -> [Int]
 findAllCost nums = fmap (`findCost` nums) [0 .. maximum nums]
@@ -32,8 +31,8 @@ day7PartII = minimum . findAllCost' <$> day7Input
 
 day7PartI :: IO Double
 day7PartI = do
-  nums <- map fromIntegral <$>  day7Input
-  let m = median  nums
+  nums <- map fromIntegral <$> day7Input
+  let m = median nums
       cost = sum . fmap (\x -> abs (x - m)) $ nums
   pure cost
 
