322f4f7310ba9cbdcafbc8b24e27c2c6f48e1661
yuanwang <yuan.wang@workiva.com>
2022-12-02 11:13:52 -0600

    wip

--- src/Y2022/Day1.hs
+++ src/Y2022/Day1.hs
@@ -1,4 +1,4 @@
-module Y2022.Day1 (partI, TopThree, foldThree, partII) where
+module Y2022.Day1 (partI, TopThree, partII) where
 
 import Data.List.Split ( splitOn )
 import Data.Semigroup (Max(Max))
@@ -21,32 +21,39 @@ partI = do
 partII :: IO ()
 partII = do
   a <- what "data/2022/day1.txt"
-  print $ (sumTopThree . foldThree . map  ( Just . sum)) a
+  print $ (sumTopThree . foldThree . map sum ) a
 
 
-data TopThree n = TopThree ( Maybe n)  ( Maybe n)   ( Maybe n)
+newtype TopThree n = TopThree {getTopThere :: (Maybe n, Maybe n, Maybe n)}
 
+toList :: TopThree n -> [n]
+toList t = ( map fromJust . filter isJust ) [x, y, z]
+  where
+  (x, y , z) = getTopThere t
 
 appendTopThree :: (Ord n, Num n) => TopThree n -> TopThree n -> TopThree n
-appendTopThree (TopThree x y z ) (TopThree x' y' z') = case nums of
-  [] -> TopThree Nothing Nothing Nothing
-  [a] -> TopThree (Just a) Nothing Nothing
-  [a, b] -> TopThree (Just a) (Just b) Nothing
+appendTopThree l r = case nums of
+  [] -> TopThree (Nothing, Nothing, Nothing)
+  [a] -> TopThree (Just a, Nothing, Nothing)
+  [a, b] -> TopThree (Just a, Just b, Nothing)
   -- [a, b, c] -> TopThree (Just a) (Just b) (Just c)
-  (a:  b: c: _) -> TopThree (Just a) (Just b) (Just c)
+  (a:  b: c: _) -> TopThree (Just a , Just b, Just c)
 
   where
-    nums =  (sortOn Down . map fromJust . filter isJust)  [x, y, z, x', y', z']
+    nums =  sortOn Down  (toList l ++ toList r )
 
 
 instance ( Ord n, Num n) => Semigroup (TopThree n) where
    (<> ) = appendTopThree
 
 instance ( Ord n, Num n) => Monoid (TopThree n) where
-  mempty = TopThree Nothing Nothing Nothing
+  mempty = TopThree (Nothing ,Nothing, Nothing)
 
-foldThree :: (Ord n, Num n, Foldable  t) => t (Maybe n)-> TopThree n
-foldThree a = (foldMap  TopThree ) a Nothing Nothing
+wrap :: a -> (Maybe a, Maybe a, Maybe a)
+wrap x = (Just x, Nothing, Nothing )
+
+foldThree :: (Ord n, Num n) => [n] -> TopThree n
+foldThree  = foldMap  TopThree . map wrap
 
 sumTopThree :: TopThree Int -> Int
-sumTopThree (TopThree x y z) = fromMaybe 0 x + fromMaybe 0 y + fromMaybe 0 z
+sumTopThree = sum . toList
