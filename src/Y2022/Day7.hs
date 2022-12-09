9573206b3535170e25b2a33160a7a6106a978e96
yuanwang <yuan.wang@workiva.com>
2022-12-09 08:47:06 -0600

    wip

--- src/Y2022/Day7.hs
+++ src/Y2022/Day7.hs
@@ -2,7 +2,6 @@
 module Y2022.Day7 where
 
 import Data.Attoparsec.Text hiding (take)
-
 import Data.Text qualified as T
 import Control.Applicative (many, (<|>))
 data FileSystem = Directory {   _dName :: String, _content :: [FileSystem], _parent :: Maybe FileSystem } | File { _fName :: String , _size :: Int }
@@ -18,25 +17,31 @@ parserFile :: Parser Output
 parserFile = do
   size <- many1 digit
   space
-  name <- many1 letter
-  return $ FL (read size) name
+  name <- many1 (letter <|> char '.' <|> digit)
+  return $ FL (read size)  name
 
 outputParser :: Parser Output
-outputParser = (string "$ ls" >> return LS) <|> (string "$ cd " >>  CD <$> many1 anyChar )  <|> (string "dir " >>  Dir <$> many1 anyChar ) <|> parserFile
+-- outputParser = (string "$ ls" >> return LS) <|> (string "$ cd " >>  CD <$> manyTill anyChar (string "\n") )  <|> (string "dir " >>  Dir <$> manyTill anyChar (string "\n") ) <|> parserFile
 
+outputParser = (string "$ ls" >> return LS) <|> (string "$ cd " >>  CD  <$> many (letter <|> char '/' <|> char '.' ))  <|> (string "dir " >>  Dir  <$> many (letter <|> digit)) <|> parserFile
 
 outputsParser :: Parser [Output]
-outputsParser = many $ outputParser <* endOfLine
+outputsParser = many (outputParser <* endOfLine   )
 
 test :: IO ()
 test = do
 
-    input <- readFile "data/2022/day7-test.txt"
-    -- print input
-    print $ parseOnly outputsParser . T.pack $ input
+    input <- readFile "data/2022/day7.txt"
+    print   ( length .lines $ input)
+    -- print $ map (parseOnly outputParser . T.pack) $ lines input
+    print $ fmap length . parseOnly outputsParser $ T.pack input
+
     -- print $ parseOnly outputParser "$ cd /"
     -- print $ parseOnly outputParser "$ ls"
     -- print $ parseOnly outputParser "14779 cmss"
     -- print $ parseOnly outputParser "dir ctctt"
     -- print $ parseOnly outputParser "101350 gpbswq.njr"
     -- print $ parseOnly outputParser "270744 mglrchsr"
+
+buildTree :: [Output] -> FileSystem -> FileSystem
+buildTree = undefined
